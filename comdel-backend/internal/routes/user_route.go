353e258045ee199a232688b8ab9a00558921a3df
KeyzarRasya <keyzar.r.athallah@gmail.com>
2025-07-10 08:32:58 +0700

    refactor: implementing dependency injection

--- comdel-backend/internal/routes/user_route.go
+++ comdel-backend/internal/routes/user_route.go
@@ -6,28 +6,52 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-func UserRoute(app *fiber.App) {
+type Router interface {
+	UserRoute(app *fiber.App) error;
+}
+
+type Route struct {
+	UserHandlers handlers.UserHandlers
+	VideoHandlers handlers.VideoHandlers
+	SubscriptionMiddleware middleware.SubscriptionMiddleware
+	PaymentHandlers handlers.PaymentHandlers
+}
+
+func NewRoute(
+	userHandlers handlers.UserHandlers,
+	videoHandlers handlers.VideoHandlers,
+	paymentHanders handlers.PaymentHandlers,
+	subscriptionMiddleware middleware.SubscriptionMiddleware,
+) Route {
+	return Route{
+		UserHandlers: userHandlers, 
+		VideoHandlers: videoHandlers,
+		PaymentHandlers: paymentHanders,
+		SubscriptionMiddleware: subscriptionMiddleware,
+	}
+}
+
+func (r *Route) UserRoute(app *fiber.App) {
 	
 	app.Route("/user", func (route fiber.Router) {
-		route.Get("/", middleware.AuthenticationMiddleware, middleware.RefreshTokenMiddleware, handlers.MainHandlers);
-		route.Get("/info", middleware.AuthenticationMiddleware, middleware.RefreshTokenMiddleware, middleware.LazyUnsubscribe, handlers.GetUserInfo)
+		route.Get("/", middleware.AuthenticationMiddleware, middleware.RefreshTokenMiddleware, r.UserHandlers.Main);
+		route.Get("/info", middleware.AuthenticationMiddleware, middleware.RefreshTokenMiddleware, r.SubscriptionMiddleware.LazyUnsubscribe, r.UserHandlers.UserInfo)
 	})
 
 	app.Route("/videos", func(route fiber.Router) {
-		route.Get("/ownership", middleware.AuthenticationMiddleware, middleware.RefreshTokenMiddleware, handlers.CheckOwnership);
-		route.Get("/information", middleware.AuthenticationMiddleware, middleware.RefreshTokenMiddleware, handlers.FetchVideoInfo)
-		route.Post("/upload", middleware.AuthenticationMiddleware, middleware.RefreshTokenMiddleware, handlers.AddVideo)
-		route.Get("/comment", middleware.AuthenticationMiddleware, handlers.RequestCommentTheadsHandler)
+		route.Get("/ownership", middleware.AuthenticationMiddleware, middleware.RefreshTokenMiddleware, r.VideoHandlers.CheckOwnership);
+		route.Get("/information", middleware.AuthenticationMiddleware, middleware.RefreshTokenMiddleware, r.VideoHandlers.VideoInfo)
+		route.Post("/upload", middleware.AuthenticationMiddleware, middleware.RefreshTokenMiddleware, r.VideoHandlers.AddVideo)
 	})
 
 	app.Route("/auth", func (route fiber.Router) {
-		route.Get("/google", handlers.OAuthLoginHandler);
-		route.Get("/google/redirect", handlers.RedirectHandler)
+		route.Get("/google", r.UserHandlers.OAuthLogin);
+		route.Get("/google/redirect", r.UserHandlers.OAuthRedirect)
 	})
 
 	app.Route("/payment", func(route fiber.Router) {
-		route.Get("/pay", handlers.CreatePayment)
-		route.Get("/finish", handlers.HandleFinishPayment)
+		route.Get("/pay", r.PaymentHandlers.CreatePayment)
+		route.Get("/finish", r.PaymentHandlers.FinishPayment)
 	})
 
 }
