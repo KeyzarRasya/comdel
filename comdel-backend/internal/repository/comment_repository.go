d0efc5c1adba26a1c8c7376755fc7266e04ba23d
KeyzarRasya <keyzar.r.athallah@gmail.com>
2025-07-10 08:23:29 +0700

    refactor: adding necessary comment function and interface

--- comdel-backend/internal/repository/comment_repository.go
+++ comdel-backend/internal/repository/comment_repository.go
@@ -1,11 +1,57 @@
 package repository
 
 import (
-	"github.com/KeyzarRasya/comdel-server/internal/dto"
+	"context"
+
 	"github.com/KeyzarRasya/comdel-server/internal/model"
+	"github.com/jackc/pgx/v5"
 )
 
 type CommentRepository interface {
-	Save(comment dto.Comment, detected bool)	error		
-	GetByVideoId(videoId string)				([]model.Comment, error);
+	GetByVideoId(videoId string)				([]*model.Comment, error);
+}
+
+type CommentRepositoryImpl struct {
+	conn *pgx.Conn;
+}
+
+func NewCommentRepository(pgxConn *pgx.Conn) CommentRepository {
+	return &CommentRepositoryImpl{conn: pgxConn}
+}
+
+func (cr *CommentRepositoryImpl) GetByVideoId(videoId string) ([]*model.Comment, error) {
+	var comments []*model.Comment;
+	var yCommentId, publishedAt, channelId, channelUrl, displayName, profileUrl, textDisplay string;
+	var isDetected bool;
+
+	rows, err := cr.conn.Query(
+		context.Background(),
+		"SELECT DISTINCT y_comment_id, published_at, channel_id, author_channel_url, display_name, profile_url, text_display, isdetected FROM comments WHERE video_id=$1",
+		videoId,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = pgx.ForEachRow(rows, []any{&yCommentId, &publishedAt, &channelId, &channelUrl, &displayName, &profileUrl, &textDisplay, &isDetected}, func () error  {
+		comments = append(comments, &model.Comment{
+			Yid: yCommentId,
+			PublishedAt: publishedAt,
+			ChannelId: channelId,
+			ChannelUrl: channelUrl,
+			DisplayName: displayName,
+			ProfileUrl: profileUrl,
+			TextDisplay: textDisplay,
+			Isdetected: isDetected,
+		})
+		return nil
+	})
+
+	if err != nil {
+		return nil, err;
+	}
+
+	return comments, nil
+
 }
