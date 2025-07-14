13ccd7b47c601a1b0d89f5f98b3545c42560197a
KeyzarRasya <keyzar.r.athallah@gmail.com>
2025-07-14 08:31:35 +0700

    refactor: adjusting and adding function

--- comdel-backend/internal/repository/video_repository.go
+++ comdel-backend/internal/repository/video_repository.go
@@ -10,8 +10,15 @@ import (
 type VideoRepository interface {
 	GetById(videoId string)											(*model.Videos, error)
 	Save(tx config.DBTx, video model.Videos)								error
+	UpdateComment(tx config.DBTx, commentsId []string, videoId string, isDeleted bool) error
 	// UpdateVideo(videoId string, cooldown time.Time, userId string)	error
 }
+
+/*_, err = tx.Exec(
+		context.Background(),
+		"UPDATE videos SET deleted_comments = $1 WHERE videos_id=$2",
+		deletedCommentsId, videoId,
+	)*/
 
 type VideoRepositoryImpl struct {
 	conn config.DBConn;
@@ -41,4 +48,22 @@ func (vr *VideoRepositoryImpl) GetById(videoId string) (*model.Videos, error) {
 	).Scan(&video.Title, &video.Thumbnail, &video.Strategy, &video.Scheduler, &video.Owner);
 
 	return &video, err;
+}
+
+func (vr *VideoRepositoryImpl) UpdateComment(tx config.DBTx, commentsId []string, videoId string, isDetected bool) error {
+	var err error;
+	if isDetected {
+		_, err = tx.Exec(
+			context.Background(),
+			"UPDATE videos SET deleted_comments = $1 WHERE videos_id=$2",
+			commentsId, videoId,
+		)
+	} else {
+		_, err = tx.Exec(
+			context.Background(),
+			"UPDATE videos SET comments = $1 WHERE videos_id=$2",
+			commentsId, videoId,
+		)
+	}
+	return err
 }
